65eddd83bb6b297de141d14734e99b4f4a028494
Anjaneyulu Byrraju <AL1437@accionlabs.in>
2023-04-21 14:28:27 +0530

    test pwd

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,6 +1,6 @@
 it('Login page', function(){
     cy.visit('https://trytestingthis.netlify.app/')
     cy.get('#uname').type('test')
-    cy.get('#pwd').type('test')
+    cy.get('#pwd').type('test123')
     cy.get('[type="submit"]').click()
 })
